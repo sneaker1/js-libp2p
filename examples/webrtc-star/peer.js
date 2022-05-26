f6f551703f5e8e0392dbe63190f0d505271849ce
Matthias Schramm <matze.schramm@googlemail.com>
2022-05-26 13:01:52 +0200

    Read webrtc-server multiaddr from parameter

--- examples/webrtc-star/peer.js
+++ examples/webrtc-star/peer.js
@@ -5,9 +5,25 @@ import {Mplex} from "@libp2p/mplex";
 import {KadDHT} from "@libp2p/kad-dht";
 import wrtc from "wrtc";
 
+
+// Reading the webrtc servers multiaddr from the parameter
+var args = process.argv.slice(2);
+if (!args[0]) {
+  // No arguments given
+  console.log("=== HELP ===")
+  console.log("You have to pass the multiaddr of the webrtc-star signalling server as an argument.");
+  console.log("node peer.js /ip4/<IP>/tcp/<PORT>/ws/p2p-webrtc-star")
+  console.log("or");
+  console.log("node peer.js /dns4/<DOMAIN>/tcp/443/wss/p2p-webrtc-star")
+  process.exit(1);
+}
+else {
+  var webrtcMultiaddr = args[0];
+}
+
+
 // Creating the libp2p node
 const node = await createLibp2p( {
-  //peerId: id,
   transports: [
     new WebRTCStar({wrtc: wrtc})
   ],
@@ -19,22 +35,19 @@ const node = await createLibp2p( {
   },
   addresses: {
     listen: [
-      "/dns4/webrtc-localnet.ms101.de/tcp/443/wss/p2p-webrtc-star"
+      webrtcMultiaddr
     ],
   },
 })
 
+
 // Starting the node
 await node.start();
 
-await node.components.getTransportManager().transports.get("@libp2p/webrtc-star").discovery.start();
-node.components.getTransportManager().transports.get("@libp2p/webrtc-star").discovery.addEventListener('peer', evt => {
-    node.onDiscoveryPeer(evt);
-});
-
-// Register handlers
-node.addEventListener("peer:discovery", (evt) => {
+// Register Event handlers
+node.addEventListener("peer:discovery", async (evt) => {
   console.log("Discovered: " + evt.detail.id.toString());
+  await node.dial(evt.detail.id);
 });
 node.connectionManager.addEventListener("peer:connect", (evt) => {
   console.log("Connected: " + evt.detail.remotePeer.toString());
@@ -43,14 +56,20 @@ node.connectionManager.addEventListener("peer:disconnect", (evt) => {
   console.log("Disconnected: " + evt.detail.remotePeer.toString());
 });
 
+// Miscellaneous
+await node.components.getTransportManager().transports.get("@libp2p/webrtc-star").discovery.start();
+node.components.getTransportManager().transports.get("@libp2p/webrtc-star").discovery.addEventListener('peer', evt => {
+    node.onDiscoveryPeer(evt);
+});
 
-// Show Infos
-console.log(`Peer started with id ${node.peerId.toString()}`)
-console.log('Listening on:')
-var myMultiaddrs = node.getMultiaddrs();
-for(var i=0; i<myMultiaddrs.length; i++) {
-  console.log(myMultiaddrs[i].toString());
+
+// Show Infos on startup
+console.log("=============================================================================================")
+console.log("Peer id: \t" + node.peerId.toString());
+console.log("Webrtc server: \t" + webrtcMultiaddr);
+var myMultiaddr = node.getMultiaddrs();
+for(var i=0; i<myMultiaddr.length; i++) {
+  console.log("Listening on: \t" + myMultiaddr[i].toString());
 }
-console.log("");
-console.log("=========================================")
+console.log("=============================================================================================")
 console.log("");
